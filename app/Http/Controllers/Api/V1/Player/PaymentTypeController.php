c7f6fa7f2699b767a8c75c185812f228c8a9f642
sophia <thangneimawi@aceplussolutions.com>
2024-09-20 15:56:16 +0700

    fixed payment type

--- app/Http/Controllers/Api/V1/Player/PaymentTypeController.php
+++ app/Http/Controllers/Api/V1/Player/PaymentTypeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\V1\Player;
 
 use App\Http\Controllers\Controller;
-use App\Models\Admin\Bank;
 use App\Models\PaymentType;
 use App\Models\UserPayment;
 use App\Traits\HttpResponses;
@@ -16,11 +15,7 @@ class PaymentTypeController extends Controller
 
     public function get()
     {
-        $player = Auth::user();
-
-        $data = PaymentType::rightJoin('banks', function ($join)  {
-            $join->on('banks.payment_type_id', '=', 'payment_types.id');
-        })->where('banks.agent_id', '=', $player->agent_id)->get();
+        $data = PaymentType::all();
 
         return $this->success($data, 'Get Payment Type List');
     }
