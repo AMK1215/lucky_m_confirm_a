a24cd94bb47812887cd32e5d8636f1f86525cd13
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-26 09:44:20 +0630

    redis updated by amk

--- app/Jobs/UpdateFinicalReportJob.php
+++ app/Jobs/UpdateFinicalReportJob.php
@@ -31,133 +31,133 @@ class UpdateFinicalReportJob implements ShouldBeUnique, ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle()
-    {
-        Transaction::has('wager')->with(['payable', 'wager'])
-            ->where('is_report_generated', false)
-            ->whereIn('name', [
-                TransactionName::Stake,
-                TransactionName::Payout,
-                TransactionName::Bonus,
-                TransactionName::JackPot,
-                TransactionName::Cancel,
-                TransactionName::Rollback,
-                TransactionName::BuyIn,
-                TransactionName::BuyOut,
-            ])
-            // ->where("id", 34)
-            ->orderBy('payable_id')
-            ->chunkById(1000, function ($transactions) {
-                $items = [];
-                $ids = [];
+    // public function handle()
+    // {
+    //     Transaction::has('wager')->with(['payable', 'wager'])
+    //         ->where('is_report_generated', false)
+    //         ->whereIn('name', [
+    //             TransactionName::Stake,
+    //             TransactionName::Payout,
+    //             TransactionName::Bonus,
+    //             TransactionName::JackPot,
+    //             TransactionName::Cancel,
+    //             TransactionName::Rollback,
+    //             TransactionName::BuyIn,
+    //             TransactionName::BuyOut,
+    //         ])
+    //         // ->where("id", 34)
+    //         ->orderBy('payable_id')
+    //         ->chunkById(1000, function ($transactions) {
+    //             $items = [];
+    //             $ids = [];
 
-                foreach ($transactions as $transaction) {
-                    $wager = $transaction->wager;
+    //             foreach ($transactions as $transaction) {
+    //                 $wager = $transaction->wager;
 
-                    if ($wager->status == WagerStatus::Ongoing) {
-                        continue;
-                    }
+    //                 if ($wager->status == WagerStatus::Ongoing) {
+    //                     continue;
+    //                 }
 
-                    if ($transaction->payable->type == UserType::Admin) {
-                        continue;
-                    }
+    //                 if ($transaction->payable->type == UserType::Admin) {
+    //                     continue;
+    //                 }
 
-                    $user_tree = UserTreeService::getUserTree($transaction->payable);
+    //                 $user_tree = UserTreeService::getUserTree($transaction->payable);
 
-                    $ids[] = $transaction->id;
+    //                 $ids[] = $transaction->id;
 
-                    $report_date = $this->reportDate($wager->created_at);
+    //                 $report_date = $this->reportDate($wager->created_at);
 
-                    foreach ($user_tree as $user_tree_item) {
-                        if (! $user_tree_item) {
-                            continue;
-                        }
+    //                 foreach ($user_tree as $user_tree_item) {
+    //                     if (! $user_tree_item) {
+    //                         continue;
+    //                     }
 
-                        $key = $report_date.'_'.$user_tree_item->parent_id;
-                        if (! isset($items[$key])) {
-                            $items[$key] = [
-                                'date' => $report_date,
-                                'user_id' => $user_tree_item->parent_id,
-                                'turnover' => 0,
-                                'payout' => 0,
-                                'win_lose' => 0,
-                                'commission' => 0,
-                                'parent_commission' => 0,
-                            ];
-                        }
+    //                     $key = $report_date.'_'.$user_tree_item->parent_id;
+    //                     if (! isset($items[$key])) {
+    //                         $items[$key] = [
+    //                             'date' => $report_date,
+    //                             'user_id' => $user_tree_item->parent_id,
+    //                             'turnover' => 0,
+    //                             'payout' => 0,
+    //                             'win_lose' => 0,
+    //                             'commission' => 0,
+    //                             'parent_commission' => 0,
+    //                         ];
+    //                     }
 
-                        $amount = abs($transaction->amount);
+    //                     $amount = abs($transaction->amount);
 
-                        // TODO: check commission amount
+    //                     // TODO: check commission amount
 
-                        if (
-                            $transaction->name == TransactionName::Stake
-                        ) {
-                            $items[$key]['turnover'] += $amount;
-                        }
-                        if (
-                            in_array($transaction->name, [
-                                TransactionName::Payout,
-                                TransactionName::Bonus,
-                                TransactionName::JackPot,
-                                TransactionName::Cancel,
-                                TransactionName::BuyOut,
-                            ])
-                        ) {
-                            $items[$key]['payout'] += $amount;
-                        }
+    //                     if (
+    //                         $transaction->name == TransactionName::Stake
+    //                     ) {
+    //                         $items[$key]['turnover'] += $amount;
+    //                     }
+    //                     if (
+    //                         in_array($transaction->name, [
+    //                             TransactionName::Payout,
+    //                             TransactionName::Bonus,
+    //                             TransactionName::JackPot,
+    //                             TransactionName::Cancel,
+    //                             TransactionName::BuyOut,
+    //                         ])
+    //                     ) {
+    //                         $items[$key]['payout'] += $amount;
+    //                     }
 
-                        if (
-                            in_array($transaction->name, [
-                                TransactionName::BuyIn,
-                                TransactionName::Rollback,
-                            ])
-                        ) {
-                            $items[$key]['payout'] -= $amount;
-                        }
+    //                     if (
+    //                         in_array($transaction->name, [
+    //                             TransactionName::BuyIn,
+    //                             TransactionName::Rollback,
+    //                         ])
+    //                     ) {
+    //                         $items[$key]['payout'] -= $amount;
+    //                     }
 
-                        $items[$key]['win_lose'] = $items[$key]['payout'] - $items[$key]['turnover'];
-                    }
-                }
+    //                     $items[$key]['win_lose'] = $items[$key]['payout'] - $items[$key]['turnover'];
+    //                 }
+    //             }
 
-                // dd($ids);
+    //             // dd($ids);
 
-                $report_dates = collect($items)->pluck('date');
+    //             $report_dates = collect($items)->pluck('date');
 
-                $existing_report_keys = FinicalReport::whereIn('date', $report_dates)->select(DB::raw('CONCAT(date, "_", user_id) as unique_report_key'))->pluck('unique_report_key')->toArray();
+    //             $existing_report_keys = FinicalReport::whereIn('date', $report_dates)->select(DB::raw('CONCAT(date, "_", user_id) as unique_report_key'))->pluck('unique_report_key')->toArray();
 
-                $new_reports = collect($items)->filter(function ($item) use ($existing_report_keys) {
-                    return ! in_array($item['date'].'_'.$item['user_id'], $existing_report_keys);
-                })->values();
+    //             $new_reports = collect($items)->filter(function ($item) use ($existing_report_keys) {
+    //                 return ! in_array($item['date'].'_'.$item['user_id'], $existing_report_keys);
+    //             })->values();
 
-                if ($new_reports->count() > 0) {
-                    FinicalReport::insert($new_reports->toArray());
-                }
+    //             if ($new_reports->count() > 0) {
+    //                 FinicalReport::insert($new_reports->toArray());
+    //             }
 
-                $update_reports = collect($items)->filter(function ($item) use ($existing_report_keys) {
-                    return in_array($item['date'].'_'.$item['user_id'], $existing_report_keys);
-                })->values();
+    //             $update_reports = collect($items)->filter(function ($item) use ($existing_report_keys) {
+    //                 return in_array($item['date'].'_'.$item['user_id'], $existing_report_keys);
+    //             })->values();
 
-                foreach ($update_reports as $update_report) {
-                    $date = $update_report['date'];
-                    $user_id = $update_report['user_id'];
+    //             foreach ($update_reports as $update_report) {
+    //                 $date = $update_report['date'];
+    //                 $user_id = $update_report['user_id'];
 
-                    unset($update_report['date']);
-                    unset($update_report['user_id']);
+    //                 unset($update_report['date']);
+    //                 unset($update_report['user_id']);
 
-                    FinicalReport::where('date', $date)->where('user_id', $user_id)->incrementEach($update_report);
-                }
+    //                 FinicalReport::where('date', $date)->where('user_id', $user_id)->incrementEach($update_report);
+    //             }
 
-                Transaction::whereIn('id', $ids)->update([
-                    'is_report_generated' => true,
-                ]);
-            });
-    }
+    //             Transaction::whereIn('id', $ids)->update([
+    //                 'is_report_generated' => true,
+    //             ]);
+    //         });
+    // }
 
-    private function reportDate($date)
-    {
-        $date->setTimezone('Asia/Yangon');
+    // private function reportDate($date)
+    // {
+    //     $date->setTimezone('Asia/Yangon');
 
-        return $date->format('Y-m-d');
-    }
+    //     return $date->format('Y-m-d');
+    // }
 }
