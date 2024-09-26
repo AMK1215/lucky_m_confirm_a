e23e0f447c5adb7549fcac36a37025b056393ca5
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-26 08:45:49 +0630

    redis updated by amk

--- app/Jobs/SyncWalletBalanceToDatabase.php
+++ app/Jobs/SyncWalletBalanceToDatabase.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SyncWalletBalanceToDatabase implements ShouldQueue
 {
@@ -19,14 +20,49 @@ class SyncWalletBalanceToDatabase implements ShouldQueue
         // Get all user IDs
         $users = DB::table('users')->pluck('id');
 
+        Log::info('SyncWalletBalanceToDatabase job started', ['user_count' => count($users)]);
+
         foreach ($users as $userId) {
             $walletKey = "wallet_balance_user_{$userId}";
 
             // Fetch balance from Redis
             $balance = Redis::get($walletKey);
 
-            // Update balance in the database
-            DB::table('wallets')->where('holder_id', $userId)->update(['balance' => $balance]);
+            if ($balance !== null) {
+                // Update balance in the database
+                DB::table('wallets')->where('holder_id', $userId)->update(['balance' => $balance]);
+
+                Log::info('Wallet balance synced', [
+                    'user_id' => $userId,
+                    'balance' => $balance,
+                ]);
+            } else {
+                // Log if the balance was not found in Redis
+                Log::warning('No wallet balance found in Redis', ['user_id' => $userId]);
+            }
         }
+
+        Log::info('SyncWalletBalanceToDatabase job completed');
     }
 }
+
+// class SyncWalletBalanceToDatabase implements ShouldQueue
+// {
+//     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+//     public function handle()
+//     {
+//         // Get all user IDs
+//         $users = DB::table('users')->pluck('id');
+
+//         foreach ($users as $userId) {
+//             $walletKey = "wallet_balance_user_{$userId}";
+
+//             // Fetch balance from Redis
+//             $balance = Redis::get($walletKey);
+
+//             // Update balance in the database
+//             DB::table('wallets')->where('holder_id', $userId)->update(['balance' => $balance]);
+//         }
+//     }
+// }
